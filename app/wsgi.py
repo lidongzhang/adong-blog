55995e424260d50344cd523fe2f2575abc8f4246
adong <lidongzhang@rstone.com.cn>
2016-09-14 18:19:34 +0800

    fix deploy

--- app/wsgi.py
+++ app/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+from os.path import join,dirname,abspath
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+import sys
+sys.path.insert(0,PROJECT_DIR)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 application = get_wsgi_application()
