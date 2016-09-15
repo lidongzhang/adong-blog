040e24764d3576ff46d0c6a7cb111bb84256b485
adong <lidongzhang@rstone.com.cn>
2016-09-15 12:53:35 +0800

    fix publish bash

--- dopull.sh
+++ dopull.sh
@@ -3,5 +3,7 @@ cd $(dirname $0)
 git stash
 git pull
 
-cat app/settings.py | sed "s/'USER': 'sa',/'USER': 'blogRoot',/g" > app/settings.py 
-cat app/settings.py | sed "s/'PASSWORD': 'sa',/'PASSWORD': 'blogRoot,123',/g" > app/settings.py
+mv  app/settings.py   app/settings.py.1
+cat app/settings.py.1 | sed "s/'USER': 'sa',/'USER': 'blogRoot',/g" > app/settings.py 
+mv  app/settings.py   app/settings.py.1
+cat app/settings.py.1 | sed "s/'PASSWORD': 'sa',/'PASSWORD': 'blogRoot,123',/g" > app/settings.py
